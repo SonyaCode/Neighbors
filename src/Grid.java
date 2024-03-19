f1840061c71e4d3bd2595df49180d191e16b5200
SonyaCode <Sonya.Zh2018@gmail.com>
2024-03-18 22:41:25 -0400

    Commit

--- src/Grid.java
+++ src/Grid.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class Grid {
+public class Grid {
+    private int[][] grid;
+
+
+    public Grid() {
+        grid = new int[5][5];
+    }
+
+    public int roll() {
+        return (int) (Math.random() * 10) + 1;
+    }
+
+    public boolean isEmpty(int row, int col) {
+        if (grid[row][col] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addNum(int num, int row, int col) {
+        grid[row][col] = num;
+    }
+
+    public int calculateScore() {
+        int score = 0;
+        int consecutiveCount = 1;
+
+        // horizontal
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length - 1; col++) {
+                if (grid[row][col] == grid[row][col + 1]) {
+                    consecutiveCount++;
+                } else if (consecutiveCount > 1) {
+                    score += consecutiveCount * grid[row][col];
+                    consecutiveCount = 1;
+                }
+            }
+            if (consecutiveCount > 1) {
+                score += consecutiveCount * grid[row][grid.length - 1];
+                consecutiveCount = 1;
+            }
+        }
+
+
+        // vertical
+        for (int col = 0; col < grid[0].length; col++) {
+            for (int row = 0; row < grid.length - 1; row++) {
+                if (grid[row][col] == grid[row + 1][col]) {
+                    consecutiveCount++;
+                } else if (consecutiveCount > 1) {
+                    score += consecutiveCount * grid[row][col];
+                    consecutiveCount = 1;
+                }
+            }
+
+            if (consecutiveCount > 1) {
+                score += consecutiveCount * grid[grid.length - 1][col];
+                consecutiveCount = 1;
+            }
+        }
+
+        return score;
+    }
+
+    public String toString() {
+        String gridForm = "    0 1 2 3 4\n   ----------\n";
+
+        for (int row = 0; row < grid.length; row++) {
+            gridForm += row + " | ";
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 0) {
+                    gridForm += "_ ";
+                } else {
+                    gridForm += grid[row][col] + " ";
+                }
+
+            }
+
+            gridForm += "\n";
+        }
+
+        return gridForm;
+    }
+
+
+
+
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        Grid grid = new Grid();
+        int boxesFilled = 0;
+        System.out.println(grid);
+
+        while (boxesFilled < 25) {
+            int dice = grid.roll();
+            System.out.println("You rolled a 10-sided dice. Dice: " + dice);
+            System.out.print("Where do you want to put the number " + dice + " at (row, column)? ");
+            String input = scan.nextLine();
+
+            int row = convertRowToInt(input);
+            int column = convertColToInt(input);
+
+
+            while (!grid.isEmpty(row, column)) {
+                System.out.println("This space is already occupied.");
+                System.out.print("Where do you want to put the number " + dice + " at (row, column)? ");
+                input = scan.nextLine();
+                row = convertRowToInt(input);
+                column = convertColToInt(input);
+
+            }
+            if (grid.isEmpty(row, column)) {
+                grid.addNum(dice, row, column);
+                boxesFilled++;
+            }
+
+            System.out.println(grid);
+
+        }
+
+        System.out.println("Your total score: " + grid.calculateScore());
+    }
+
+    public static int convertRowToInt(String row) {
+        String rowInStr = row.substring(0, row.indexOf(","));
+        return Integer.parseInt(rowInStr);
+    }
+
+    public static int convertColToInt(String column) {
+        String colInStr = column.substring(column.indexOf(",") + 2);
+        return Integer.parseInt(colInStr);
     }
 }
